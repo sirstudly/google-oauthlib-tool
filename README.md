7b17b5d449a3dee0406ad7e6becf607f057ce8e3
sirstudly <ron1404@techie.com>
2024-11-22 17:39:56 +0000

    Updated endpoint for local vs ec2"

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ Registers a new "device" with Google Assistant privileges.
 Once setup, allows a nodeJS script to be run to execute commands against Google Assistant.
 
 See https://github.com/googlesamples/assistant-sdk-nodejs
+
+
+# install nvm/nodejs for googleassist 
+# https://linuxize.com/post/how-to-install-node-js-on-centos-7/
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+nvm install --lts
+sudo yum install gcc-c++ make
+cd google-assistant-gprc
+npm install
+node timecheck.js
--- google-assistant-grpc/html/googleassist.php
+++ google-assistant-grpc/html/googleassist.php
@@ -3,16 +3,32 @@
 // googleassist.sh needs to be runnable as sudo as the apache user
 // ie. sudo visudo
 // apache ALL=(ALL) NOPASSWD: /home/ec2-user/google-assistant-grpc/googleassist.sh
+// Above is not required when running locally (Ubuntu)
 header('Content-Type: application/json');
-chdir('/home/ec2-user/google-assistant-grpc');
+chdir('/usr/local/grpc/google-assistant-grpc');
+
 if( $_GET['command'] == 'projectoron' ) {
-    $output = trim(shell_exec( 'sudo /home/ec2-user/google-assistant-grpc/googleassist.sh "turn projector on"' ));
+    $output = trim(shell_exec( '/usr/local/grpc/google-assistant-grpc/googleassist.sh "turn projector on"' ));
     echo ($output == "undefined" ? '{ "message": "ok" }' : '{ "message": ' . json_encode($output) . ' }');
 } elseif( $_GET['command'] == 'projectoroff' ) {
-    $output = trim(shell_exec( 'sudo /home/ec2-user/google-assistant-grpc/googleassist.sh "lights out"' ));
+    $output = trim(shell_exec( '/usr/local/grpc/google-assistant-grpc/googleassist.sh "lights out"' ));
+    sleep(3);
+    $output = trim(shell_exec( '/usr/local/grpc/google-assistant-grpc/googleassist.sh "lights out"' ));
+    sleep(3);
+    $output = trim(shell_exec( '/usr/local/grpc/google-assistant-grpc/googleassist.sh "lights out"' ));
+    echo ($output == "undefined" ? '{ "message": "ok" }' : '{ "message": ' . json_encode($output) . ' }');
+} elseif( $_GET['command'] == 'restart_wan9' ) {
+    $output = trim(shell_exec( '/usr/local/grpc/google-assistant-grpc/googleassist.sh "turn off wan9"' ));
+    sleep(5);
+    $output = trim(shell_exec( '/usr/local/grpc/google-assistant-grpc/googleassist.sh "turn on wan9"' ));
+    echo ($output == "undefined" ? '{ "message": "ok" }' : '{ "message": ' . json_encode($output) . ' }');
+} elseif( $_GET['command'] == 'restart_wan10' ) {
+    $output = trim(shell_exec( '/usr/local/grpc/google-assistant-grpc/googleassist.sh "turn off wan10"' ));
+    sleep(5);
+    $output = trim(shell_exec( '/usr/local/grpc/google-assistant-grpc/googleassist.sh "turn on wan10"' ));
     echo ($output == "undefined" ? '{ "message": "ok" }' : '{ "message": ' . json_encode($output) . ' }');
 } elseif( $_GET['command'] == 'time' ) {
-    $output = trim(shell_exec( 'sudo /home/ec2-user/google-assistant-grpc/googleassist.sh "what time is it?"' ));
+    $output = trim(shell_exec( '/usr/local/grpc/google-assistant-grpc/googleassist.sh "what time is it?"' ));
     echo ($output == "undefined" ? '{ "message": "ok" }' : '{ "message": ' . json_encode($output) . ' }');
 } else {
     echo '{ "message": "bugger off!" }';
